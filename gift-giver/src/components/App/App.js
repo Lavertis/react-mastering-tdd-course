f9a9b8c8327eba5129cf490e6192ab78b6edc180
Rafał Kuźmiczuk <29688908+Lavertis@users.noreply.github.com>
2022-06-12 17:17:45 +0200

    10. Render the Gift List in the App

--- gift-giver/src/components/App/App.js
+++ gift-giver/src/components/App/App.js
@@ -1,9 +1,33 @@
-function App() {
-    return (
-        <div className="App">
-            <h2>Gift Giver</h2>
-        </div>
-    );
+import {Component} from "react";
+import {Button} from "react-bootstrap";
+
+class App extends Component {
+    constructor() {
+        super();
+
+        this.state = {gifts: []}
+    }
+
+    addGift = () => {
+        const {gifts} = this.state;
+        const ids = gifts.map(gift => gift.id);
+        const maxId = ids.length > 0 ? Math.max(...ids) : 0;
+        gifts.push({id: maxId + 1})
+        this.setState({gifts});
+    }
+
+    render() {
+        return (
+            <div className="App">
+                <h2>Gift Giver</h2>
+                <Button
+                    className="btn-add"
+                    onClick={this.addGift}>
+                    Add Gift
+                </Button>
+            </div>
+        );
+    }
 }
 
 export default App;
--- gift-giver/src/components/App/App.test.js
+++ gift-giver/src/components/App/App.test.js
@@ -1,3 +1,4 @@
+import React from "react";
 import App from './App';
 import {shallow} from "enzyme";
 
@@ -5,4 +6,14 @@ const app = shallow(<App/>)
 
 it('renders correctly', () => {
     expect(app).toMatchSnapshot();
+});
+
+it('initializes the `state` with a empty list of gifts', () => {
+    jest.spyOn(React, 'useState')
+    expect(app.state().gifts).toEqual([])
+});
+
+it('adds a new gift to `state` when clicking the `add gift` button', () => {
+    app.find('.btn-add').simulate('click');
+    expect(app.state().gifts).toEqual([{id: 1}])
 });
