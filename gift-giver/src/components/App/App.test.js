d6574b18a9471950e81d4ea972ad0b80a79f2636
Rafał Kuźmiczuk <29688908+Lavertis@users.noreply.github.com>
2022-06-12 17:48:55 +0200

    13. BDD in GiftGiver

--- gift-giver/src/components/App/App.test.js
+++ gift-giver/src/components/App/App.test.js
@@ -2,23 +2,34 @@ import React from "react";
 import App from './App';
 import {shallow} from "enzyme";
 
-const app = shallow(<App/>)
 
-it('renders correctly', () => {
-    expect(app).toMatchSnapshot();
+describe('App', () => {
+    const app = shallow(<App/>)
+
+    it('renders correctly', () => {
+        expect(app).toMatchSnapshot();
+    });
+
+    it('initializes the `state` with a empty list of gifts', () => {
+        jest.spyOn(React, 'useState')
+        expect(app.state().gifts).toEqual([])
+    });
+
+    describe('when clicking the `add gift` button', () => {
+        beforeEach(() => {
+            app.find('.btn-add').simulate('click');
+        });
+
+        afterEach(() => {
+            app.setState({gifts: []})
+        });
+
+        it('adds a new gift to `state`', () => {
+            expect(app.state().gifts).toEqual([{id: 1}]);
+        });
+
+        it('adds a new gift to the rendered list', () => {
+            expect(app.find('.gift-list').children()).toHaveLength(1);
+        })
+    });
 });
-
-it('initializes the `state` with a empty list of gifts', () => {
-    jest.spyOn(React, 'useState')
-    expect(app.state().gifts).toEqual([])
-});
-
-it('adds a new gift to `state` when clicking the `add gift` button', () => {
-    app.find('.btn-add').simulate('click');
-    expect(app.state().gifts).toEqual([{id: 1}]);
-});
-
-it('adds a new gift to the rendered list when clicking the `add gift` button', () => {
-    app.find('.btn-add').simulate('click');
-    expect(app.find('.gift-list').children()).toHaveLength(2);
-})
