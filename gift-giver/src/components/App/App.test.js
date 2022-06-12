65704b1e7c796a2401d6aabcde8e3811121ddc59
Rafał Kuźmiczuk <29688908+Lavertis@users.noreply.github.com>
2022-06-12 17:34:00 +0200

    11. Render GiftGiver and the Gift List

--- gift-giver/src/components/App/App.test.js
+++ gift-giver/src/components/App/App.test.js
@@ -15,5 +15,10 @@ it('initializes the `state` with a empty list of gifts', () => {
 
 it('adds a new gift to `state` when clicking the `add gift` button', () => {
     app.find('.btn-add').simulate('click');
-    expect(app.state().gifts).toEqual([{id: 1}])
+    expect(app.state().gifts).toEqual([{id: 1}]);
 });
+
+it('adds a new gift to the rendered list when clicking the `add gift` button', () => {
+    app.find('.btn-add').simulate('click');
+    expect(app.find('.gift-list').children()).toHaveLength(2);
+})
