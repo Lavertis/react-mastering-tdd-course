fc2123d1ad2ddfe858c3aba7d8ecaf82c6064810
Rafał Kuźmiczuk <29688908+Lavertis@users.noreply.github.com>
2022-06-13 10:53:23 +0200

    31. Dispatch Deposit and Withdraw

--- loot-check/src/components/Wallet/Wallet.js
+++ loot-check/src/components/Wallet/Wallet.js
@@ -1,14 +1,31 @@
 import React, {Component} from 'react';
 import {connect} from "react-redux";
+import {deposit, withdraw} from "../../actions/balance/balance";
 
 
 export class Wallet extends Component {
+    constructor() {
+        super();
+
+        this.state = {
+            balance: undefined
+        };
+    }
+
+    updateBalance = (event) => this.setState({balance: parseInt(event.target.value, 10)});
+    deposit = () => this.props.deposit(this.state.balance);
+    withdraw = () => this.props.withdraw(this.state.balance);
+
     render() {
         return (
             <div>
                 <h3 className="balance">
                     Wallet balance: {this.props.balance}
                 </h3>
+                <br/>
+                <input className="input-wallet" onChange={(e) => this.updateBalance(e)}/>
+                <button className="btn-deposit" onClick={this.deposit}>Deposit</button>
+                <button className="btn-withdraw" onClick={this.withdraw}>Withdraw</button>
             </div>
         );
     }
@@ -18,4 +35,4 @@ const mapStateToProps = state => {
     return {balance: state.balance};
 };
 
-export default connect(mapStateToProps)(Wallet);
+export default connect(mapStateToProps, {deposit, withdraw})(Wallet);
