856a84616a75db20bda2b0717c6de5eb92e3648e
Rafał Kuźmiczuk <29688908+Lavertis@users.noreply.github.com>
2022-06-16 15:04:45 +0200

    37. Fetch the Bitcoin Value through Redux

--- loot-check/src/actions/actionTypes.js
+++ loot-check/src/actions/actionTypes.js
@@ -1,7 +1,8 @@
 const actionTypes = {
     SET_BALANCE: 'SET_BALANCE',
     DEPOSIT: 'DEPOSIT',
-    WITHDRAW: 'WITHDRAW'
+    WITHDRAW: 'WITHDRAW',
+    FETCH_BITCOIN: 'FETCH_BITCOIN'
 };
 
 export default actionTypes;
